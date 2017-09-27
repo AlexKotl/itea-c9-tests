c70a252d53ae20316e1e2ca6f0b4ef8ec0fe8506
slicer <slicer256@gmail.com>
2017-09-27 17:57:06 +0000

    controller rename

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -4,25 +4,25 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class BlogControllerController extends Controller
+class BlogController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AppBundle:BlogController:index.html.twig', array(
+        return $this->render('AppBundle:Blog:index.html.twig', array(
             // ...
         ));
     }
 
     public function viewAction()
     {
-        return $this->render('AppBundle:BlogController:view.html.twig', array(
+        return $this->render('AppBundle:Blog:view.html.twig', array(
             // ...
         ));
     }
 
     public function addAction()
     {
-        return $this->render('AppBundle:BlogController:add.html.twig', array(
+        return $this->render('AppBundle:Blog:add.html.twig', array(
             // ...
         ));
     }
