93a7a31d9c75913dc180cac47d80cbb3d49a102b
slicer <slicer256@gmail.com>
2017-09-28 20:11:17 +0000

    menu moved to separate action

--- src/ShopBundle/Controller/DefaultController.php
+++ src/ShopBundle/Controller/DefaultController.php
@@ -9,8 +9,29 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ShopBundle:Default:index.html.twig', [
+        return $this->render('ShopBundle:Default:index.html.twig');
+    }
+    
+    public function menuAction() 
+    {
+        return $this->render('ShopBundle:Default:menu.html.twig', [
             'menu' => DataController::$data['categories']
+        ]);
+    }
+    
+    public function categoryAction($name) 
+    {
+        $category = array_filter(
+            DataController::$data['categories'],
+            function($e) use ($name) {
+                return $e['title'] === $name;
+            }
+        );
+        $category = array_pop($category);
+        
+        return $this->render('ShopBundle:Default:category.html.twig', [
+            'items' => DataController::$data['categories'],
+            'category' => $category,
         ]);
     }
 }
