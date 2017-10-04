a684b6492fc6008c10ffab389f66fc41eb26765f
slicer <slicer256@gmail.com>
2017-10-04 18:11:45 +0000

    create first one-many relation

--- src/AppBundle/Entity/Product.php
+++ src/AppBundle/Entity/Product.php
@@ -113,5 +113,33 @@ class Product
     public function getNameAndPrice() {
         return $this->name . ' and ' . $this->price;
     }
-}
+    /**
+     * @var \AppBundle\Entity\Category
+     */
+    private $category;
 
+
+    /**
+     * Set category
+     *
+     * @param \AppBundle\Entity\Category $category
+     *
+     * @return Product
+     */
+    public function setCategory(\AppBundle\Entity\Category $category = null)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return \AppBundle\Entity\Category
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+}
