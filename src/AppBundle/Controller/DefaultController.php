411f2f56a1721005dcb470ca2da09b781deb5d8d
slicer <slicer256@gmail.com>
2017-10-04 18:23:55 +0000

    save static product and category

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Product;
+use AppBundle\Entity\Category;
 
 class DefaultController extends Controller
 {
@@ -13,14 +15,31 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        
+        
+        
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        // return $this->render('default/index.html.twig', [
+        //     'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        // ]);
     }
     
     public function blogAction($page) {
-        die("BLOG page {$page}");  
+        $category = new Category();
+        $category->setName("new category");
+        
+        $product = new Product();
+        $product->setName("First");
+        $product->setPrice(99);
+        $product->setDescription("Description");
+        $product->setCategory($category);
+        
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($category);
+        $em->persist($product);
+        $em->flush();
+        
+        return $this->render('base.html.twig');
     }
     
     public function newsAction($name = '', $page = 1, $order = '', $sort = '', $lang = 'ua') {
