85aa7aeab7c1a84616063a0ef335de3bc5368b79
slicer <slicer256@gmail.com>
2017-10-09 18:40:26 +0000

    very simple form

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,14 +2,28 @@
 
 namespace AppBundle\Controller;
 
+use BlogBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class UserController extends Controller
 {
-    public function registerAction()
+    public function registerAction(Request $request)
     {
+        $user = new User();
+        
+        $form = $this->createFormBuilder($user)
+            ->add('first_name')
+            ->add('last_name')
+            ->add('email')
+            ->add('submit', SubmitType::class)
+            ->getForm();
+            
+        $form->handleRequest($request);
+        
         return $this->render('AppBundle:User:register.html.twig', array(
-            // ...
+            'form' => $form->createView()
         ));
     }
 
